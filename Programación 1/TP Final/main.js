182a21ad56727a88a5996c523acb3a7c9b530690
Nacho <nachomigoni@gmail.com>
2022-11-19 00:08:12 -0300

    Incio2

--- Programación 1/TP Final/main.js
+++ Programación 1/TP Final/main.js
@@ -1,11 +1,11 @@
 class Clinica{
-    constructor(nombre,dueno,r_juridica,enfermeros,pacientes){
+    constructor(nombre,dueño,r_juridica,enfermeros,pacientes){
         this.nombre = nombre
-        this.dueno = dueno
+        this.dueño = dueño
         this.r_juridica = r_juridica
-        this.doctores = doctores
-        this.enfermeros = enfermeros
-        this.pacientes = pacientes
+        this.doctores = doctores // Ver array de doctores
+        this.enfermeros = enfermeros // Ver array de enfermeros
+        this.pacientes = pacientes 
     };
     muestra(){ //tomar un elemento html y enviar los datos de la clinica alli
         
@@ -20,7 +20,8 @@ class Personal{
         this._pacientes = pacientes
     };
 }
-
+ // Crear un metodo para agregar paciente
+ //Utilizar un arreglo para mapear el arreglo de pacientes y redirigirlo a un arreglo de pacientes de x doctor
 class Doctores{
     constructor(nombre,apellido,matricula,pacientes,especialidad){
         super(nombre,apellido,matricula,pacientes);
@@ -57,16 +58,27 @@ class Paciente{
 
     }
 }
+
+//Inicio del programa
+//Definicion de arreglos
+// 1- Crear los arreglos que contendrán los datos
 let doctores = []
 let enfermeros = []
 let pacientes = []
 
-function carga(){
+// 2° Mostrar los doctores en el select
+let select = document.getElementById("medenf").innerHTML // Toma el select del html
+doctores.forEach(element => {
+    select =+ `<option>${element}</option>`
+});
+
+// 3° Obtener los datos
+function carga(){ // Funcion que toma los datos del paciente luego de pulsar el boton
     let nombreC = document.getElementById("nombreC").value
     let apellidoC = document.getElementById("apellidoC").value
     let dni = document.getElementById("dni").value
     let f_nacimiento = document.getElementById("f_nacimiento").value
     let medico = document.getElementById("medico").value
     let o_social = document.getElementById("o_social").value
-    let paciente = new Paciente(nombreC,apellidoC,dni,f_nacimiento,medico,o_social)
+    let paciente = new Paciente(nombreC,apellidoC,dni,f_nacimiento,medico,o_social) // Crea el objeto paciente y enviar los datos a la clase Paciente
 }
