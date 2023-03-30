c00718f67a85d720f7c8af825307d2f9f649010e
Nacho <nachomigoni@gmail.com>
2023-03-30 20:52:56 -0300

    Inicio ej3

--- Segundo Año/Programación Backend/TP repaso/Ejercicio2.js
+++ Segundo Año/Programación Backend/TP repaso/Ejercicio2.js
@@ -1,3 +1,16 @@
 let caracteres = "murcielago";
-// Para enviar datos a una funcion, enviarlos como parámetros
+// Apunte: Para enviar datos a una funcion, enviarlos como parámetros
 
+let detectar_vocales = (array_inicial) => {
+    let solo_vocales = [];
+    for (let index = 0; index < array_inicial.length; index++) {
+        const element = array_inicial[index];
+        if (element == "a" | element == "e" | element == "i" | element == "o" | element == "u"){
+            solo_vocales.push(element);
+        }
+    }
+    return(solo_vocales);
+}
+
+console.log("El arreglo inicial es: "+'"'+caracteres+'"')
+console.log("Las vocales del array son: ["+detectar_vocales(caracteres)+"]");
---
+++ Segundo Año/Programación Backend/TP repaso/Ejercicio3.js
@@ -0,0 +1,11 @@
+let email = "nm@g.com"
+
+let validar = (mail) => {
+for (let index = 0; index < mail.length; index++) {
+    const element = mail[index];
+    if(element == "@"){
+        // desde la posicion del arroba hasta el final, es el dominio del mail
+        let dominio = mail.substring()
+    }
+}
+}
