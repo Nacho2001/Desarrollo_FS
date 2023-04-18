5a7aa54e7f8f9b720ff93aa7d3468470da185c11
Nacho <nachomigoni@gmail.com>
2023-04-18 20:43:04 -0300

    completar metodo delete

--- Segundo Año/Integración de Aplicaciones/Clase2/routes/product_routes.js
+++ Segundo Año/Integración de Aplicaciones/Clase2/routes/product_routes.js
@@ -4,6 +4,7 @@ const path = require('path'); // Arbol de directorios de los archivos
 const router = Router(); // Define router
 const pathData = path.join(__dirname, '/../data/products.json'); // Obtiene ruta completa hacia el archivo JSON
 
+// Obtener todos los elementos
 router.get('/', (req, res) => {
 	fs.readFile(pathData, 'utf8', (err,resp) => {
 		data = JSON.parse(resp);
@@ -12,10 +13,50 @@ router.get('/', (req, res) => {
 		})
 	})
 })
-
-router.post('/addproduct', (req,res) => {
+// Enviar elemento
+router.post('/addproduct/:id', (req,res) => {
+	const id = req.params.id
 	fs.readFile(pathData, 'utf8', (err, resp) => {
+		const dataClient = req.body
 		data = JSON.parse(resp);
+		data[id] = dataClient;
+		res.status(201).json({
+			data
+		})
+	})
+})
+// Busca producto por id
+router.get('/myproduct/:id', (req, res) => {
+	const id = req.params.id
+	fs.readFile(pathData, 'utf8', (err,resp) => {
+		dataFull = JSON.parse(resp);
+		data = dataFull[id]
+		res.status(200).json({
+			data
+		})
+	})
+})
+// Actualizar elemento
+router.put('/update/:id', (req,res) => {
+	const id = req.params.id
+	fs.readFile(pathData,'utf8', (err,resp) => {
+		const dataProduct = req.body
+		data = JSON.parse(resp);
+		data[id] = dataProduct;
+		res.status(200).json({
+			data
+		})
+	})
+})
+//Borrar elemento
+router.delete('/delete/:id', (req,res) => {
+	const id = req.params.id
+	fs.readFile(pathData, 'utf8', (err,resp) => {
+		dataFull = JSON.parse(resp);
+		data = dataFull[id];
+		res.status(200).json({
+			data : null
+		})
 	})
 })
 
--- Segundo Año/Integración de Aplicaciones/proyecto/app.js
+++
@@ -1,6 +0,0 @@
-// Invoca el archivo donde esta la clase
-const Server = require('./server');
-// Llama a la clase
-const server = new Server;
-// Utiliza el metodo escucha
-server.escucha();
