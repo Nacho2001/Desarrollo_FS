bfa546337a8a60cee98ebfaa214f32482e6471a8
Nacho2001 <nachomigoni@gmail.com>
2023-04-21 11:29:35 -0300

    avance_ej1

--- Segundo Año/Programación Backend/TP2/ej1.js
+++ Segundo Año/Programación Backend/TP2/ej1.js
@@ -3,25 +3,35 @@ class Estudiante{
         this.nombre = nombre;
         this.apellido = apellido;
     }
-    creaObjetoEstudiante(){
+    creaEstudiante(){
         let estudiante = {nombre: this.nombre, apellido: this.apellido}
         return estudiante;
     }
 }
 
 class Curso{
-    constructor(año, carrera){
-        this.estudiante = estudiante
+    constructor(nombre,año, carrera){
+        this.nombre = nombre
+        this.año = año
+        this.carrera = carrera
     }
     listaEstudiantes = []
-    asignarEstudiante(){
 
+    asignarEstudiante(alumno){
+        this.alumno = alumno
+        alumno.push(listaEstudiantes)
+    }
+
+    crearCurso(){
+        
     }
 }
 
 curso1 = new Curso("Reparacion", 2023, "Capacitación");
 est1 = new Estudiante("pepe", "argento");
 est2 = new Estudiante("lucas", "gonzales");
+est1.asignarEstudiante();
+est2.asignarEstudiante();
 
 
 /*
