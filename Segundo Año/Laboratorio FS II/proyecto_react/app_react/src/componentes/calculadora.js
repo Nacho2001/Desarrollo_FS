f71969a385955e644b385998a3b54399ea52da51
Nacho <nachomigoni@gmail.com>
2023-06-21 20:55:30 -0300

    CompletarFormulario

--- Segundo Año/Laboratorio FS II/proyecto_react/app_react/src/componentes/calculadora.js
+++ Segundo Año/Laboratorio FS II/proyecto_react/app_react/src/componentes/calculadora.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 import './Calculadora.css'
 export default function Calculadora(){
+    const [nros, modNumeros] = useState({
+        nro1:0,
+        nro2:0,
+        resultado:0
+    })
+    const handleInputChange = (event) => {
+        modNumeros({
+            ...nros,
+            [event.target.name]: [event.target.value]
+        })
+    }
+    const enviarOperacion = (event) => {
+        event.preventDefault()
+        
+    }
     return (
         <>
         <div className="campo">
-            <label>Ingresar primer número</label>
-            <input id="nro1"></input>
-            <label>Ingresar segundo valor</label>
-            <input id="nro2"></input>
-            <label>Seleccione la operación</label>
-            <label>Resultado:</label>
+            <form onSubmit={enviarOperacion}>
+                <label>Ingresar primer número</label>
+                <input onChange={handleInputChange} name="nro1"/>
+                <label>Ingresar segundo valor</label>
+                <input onChange={handleInputChange} name="nro2"/>
+                <label>Seleccione la operación</label>
+                <select>INgresar opc</select>
+                <button type="submit">Calcular</button>
+                <label>Resultado:{resultado}</label>
+            </form>
         </div>
         </>
     );
