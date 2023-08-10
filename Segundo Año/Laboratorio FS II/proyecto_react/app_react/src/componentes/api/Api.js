3b392222eb604edf248fab7ab20e15be7cc910c6
Nacho2001 <nachomigoni@gmail.com>
2023-08-10 12:18:06 -0300

    UpdateCards

--- Segundo Año/Laboratorio FS II/proyecto_react/app_react/src/componentes/api/Api.js
+++ Segundo Año/Laboratorio FS II/proyecto_react/app_react/src/componentes/api/Api.js
@@ -1,25 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PokeCardRed from "./PokeCardRed";
 import PokeCardBlue from "./PokeCardBlue";
-import ApiData from "./ApiData";
 
-function getPokemon(id){
-    const [error,setError] = useState(null);
-    const [items, setItems] = useState([]);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${props.id}`)
-    .then(res = res.json())
-    .then((result) => { setItems(result) },
-    (error) => {setError(error)} )
-    
-}
 export default function Api(){
     let listaRoja=[984,985,986,987,988,989,1005,1007,1009];
     let listaAzul=[990,991,992,993,994,995,1006,1008,1010];
     return (
         <div className="ms-3">
         <h3 className="text-light">Pokémon Paradojas</h3>
-        <PokeCardRed/>
-        <PokeCardBlue/>
+        {listaRoja.forEach(pkid => {
+            <PokeCardRed pkid={pkid} />
+        })}
+        {/*<PokeCardBlue/>*/}
         </div>
     )
 };
--- Segundo Año/Laboratorio FS II/proyecto_react/app_react/src/componentes/api/PokeCardRed.js
+++ Segundo Año/Laboratorio FS II/proyecto_react/app_react/src/componentes/api/PokeCardRed.js
@@ -1,21 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 export default function PokeCardRed(props){
-    return (
-        <div className="card mb-3 bg-danger" style={{"max-width":" 540px"}}>
-            <div className="row">
-                <div className="col-4">
-                    <img src="" alt=""/>
-                </div>
-                <div className="col-8">
-                    <div className="card-body">
-                        <h5>Nombre</h5>
-                        <p>Altura: altura Peso:peso</p>
-                        <p>Descripcion</p>
-                        <p>descripcion</p>
+    const [error,setError] = useState(null);
+    const [data, setData] = useState([]);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${props.pkid}`)
+    .then(res => res.json())
+    .then((result) => { setData(result) },
+    (error) => {setError(error)} )
+    if (error) {
+        console.log(error)
+    } else {
+        return (
+            <div className="card mb-3 bg-danger" style={{"max-width":" 540px"}}>
+                <div className="row">
+                    <div className="col-4">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5>{data.nombre}</h5>
+                            <p>Altura: altura Peso:peso</p>
+                            <p>Descripcion</p>
+                            <p>descripcion</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 };
