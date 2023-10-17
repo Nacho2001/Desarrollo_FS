ba10e45022022f9ce79a3edfa449697e501d19a4
Nacho <nachomigoni@gmail.com>
2023-10-17 20:33:11 -0300

    prime Importado con exito

--- Segundo Año/Laboratorio FS II/proyect_prime/src/App.js
+++ Segundo Año/Laboratorio FS II/proyect_prime/src/App.js
@@ -1,25 +1,13 @@
-import Formulario from "./componentes/Formulario";
 import React, { useEffect } from "react";
-import axios from 'axios';
+import { PrimeReactProvider } from 'primereact/api'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 
-function App() {
-  let usuarios = [];
-  const obtenerDatos = () => {
-    const resp = axios.get("http://localhost:5000/usuarios")
-    .then(
-      usuarios = resp.data.usuarios
-    )
-    .error( console.error(error))
-  }
-
-  useEffect(() => {
-    obtenerDatos();
-  })
+        
+function App(){
   return (
-    <div className="App">
-      <h3>Lista de usuarios</h3>
-      <Formulario/>    
-    </div>
+    <PrimeReactProvider>
+      <InputText></InputText>
+    </PrimeReactProvider>
   );
 }
 
