f4cff9ff96be8e6a4fd20afc93d6c90ef1b939a2
Nacho2001 <nachomigoni@gmail.com>
2023-10-27 15:27:37 -0300

    Completar lista Dex

--- Segundo Año/Laboratorio FS II/prime_royect/src/componentes/tarjetaPokemon.js
+++ Segundo Año/Laboratorio FS II/prime_royect/src/componentes/tarjetaPokemon.js
@@ -2,17 +2,27 @@ import { Card } from 'primereact/card';
 
 const TarjetaPokemon = (props) => {
     const pokemonData = props.pokemon
+    const link = `https://www.wikidex.net/wiki/${pokemonData.name}`
     const imagen= `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`
+    const [types, setTypes] = useState([])
+    setTypes(pokemonData.types)
+    function tipos(){
+        let tipoElemental = ""
+        types.forEach(type => {
+             // Recorrer arreglo types y guardar los tipos en tipoElemental
+        });
+    }
     if (props.visible){
         return (
             <Card>
-                <div className="flex flex-column md:flex-row lg:flex-row xl:flex-row w-11">
+                <div className="flex flex-column md:flex-row lg:flex-row xl:flex-row">
                     <img src={imagen} width="260" height="260"/>
                     <div className="">
-                        <div><b>Nombre</b>: {pokemonData.name}</div>
-                        <div><b>Altura</b>: {pokemonData.height}</div>
-                        <div><b>Peso</b>: {pokemonData.width}</div>
-                        <div><b>Descripción</b>:{pokemonData.description}</div>
+                        <div><b>Name</b>: {pokemonData.name}</div>
+                        <div><b>Height</b>: {pokemonData.height/10} M</div>
+                        <div><b>Weight</b>: {pokemonData.weight/10} Kg</div>
+                        <div><b>Type</b>:{pokemonData.description}</div>
+                        <div><b>Reference Link</b>: <a href={link}>{pokemonData.name}</a></div>
                     </div>
                 </div>
             </Card>
