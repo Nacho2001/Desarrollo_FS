b096ec48bc7e2a37cd71631c49d2d20be9fb7c44
Nacho2001 <nachomigoni@gmail.com>
2023-11-03 15:33:35 -0300

    Publicaciones callbacks

--- Segundo Año/Programacion_Frontend/TP-backendFront/frontend/MyNet/src/callback.ts
+++ Segundo Año/Programacion_Frontend/TP-backendFront/frontend/MyNet/src/callback.ts
@@ -43,3 +43,46 @@ export const updateUser = async (id:number, username:string, password:string, em
         console.error(error);
     }
 }
+
+export const getPosts = async () => {
+    try {
+        const resp = await axios.get("http://localhost:5000/publicaciones")
+        const posts = resp.data.publicaciones
+        return posts;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const getUniquePost = async (id:number) => {
+    try {
+        const resp = await axios.get("http://localhost:5000/publicaciones"+id)
+        const post = resp.data.publicacion
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const addPost = async (titulo:string,cuerpo:string,usuario:string,fecha:Date) => {
+    try {
+        await axios.post("http://localhost:5000/publicaciones", {titulo,cuerpo,usuario,fecha})
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const deletePost = async (id:number) => {
+    try {
+        await axios.delete("http://localhost:5000/publicaciones/"+id)
+    } catch (error) {
+        
+    }
+}
+
+export const updatePost = async (id:number, titulo:string, cuerpo:string, usuario:string, fecha:Date) => {
+    try {
+        await axios.put(`http://localhost:5000/publicaciones/${id}`,{titulo,cuerpo,usuario,fecha})
+    } catch (error) {
+        console.error(error)
+    }
+}
