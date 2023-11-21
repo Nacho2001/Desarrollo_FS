12c15cef536c3435e0327812ca5aefb7c6f933a7
Nacho2001 <nachomigoni@gmail.com>
2023-11-21 12:17:42 -0300

    Completar modo edicion publicacion

--- Segundo Año/Programacion_Frontend/TP-backendFront/MyNet2/vite-project/src/pages/Muro.jsx
+++ Segundo Año/Programacion_Frontend/TP-backendFront/MyNet2/vite-project/src/pages/Muro.jsx
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 import { getUserPosts } from "../callback";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import  CardPublicacion from '../components/CardPublicacion'
+import {Button} from 'primereact/button';
+import FormPost from "../components/FormPost";
+
 const Muro = () => {
     const token = useSelector((state) => state.credencialesUsuario.credencialesUsuario.token)
     const username = useSelector((state) => state.credencialesUsuario.credencialesUsuario.username)
     const [misPost, setMisPost] = useState([]);
+    const [postMode, setPostMode] = useState("read");
     useEffect(() => {
         const getMyPost = async () => {
             setMisPost(await getUserPosts(username, token))
@@ -13,14 +17,39 @@ const Muro = () => {
         // Corregir funcion Callback
         getMyPost();
     }, [])
+    const render = () => {
+        switch (postMode) {
+            case "add":
+                return (
+                    <FormPost/>
+                )
+            case "edit":
+                return (
+                    <FormPost2/>
+                )
+            default:
+                return(
+                    <>
+                        <Button label="Añadir post" icon="pi pi-plus" severity="success"></Button>
+                        <div>
+                            {misPost.map((post) => (
+                                <CardPublicacion post={post} key={post.id}/>
+                            ))}
+                        </div>
+                    </>
+                )
+                break;
+        }
+    }
     return (
         <>
             <h2>Mi Muro</h2>
-            <ul>
+            <Button label="Añadir post" icon="pi pi-plus" severity="success"></Button>
+            <div>
                 {misPost.map((post) => (
                     <CardPublicacion post={post} key={post.id}/>
                 ))}
-            </ul>
+            </div>
         </>
     )
 }
