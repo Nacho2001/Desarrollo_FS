1979973f7530bc3aa74e04f202978f19cd1b6d17
Nacho <nachomigoni@gmail.com>
2024-04-26 21:10:17 -0300

    dotenv funcionando

--- Tercer Año/Desarrollo Web/serverProyect/api/config/db.js
+++ Tercer Año/Desarrollo Web/serverProyect/api/config/db.js
@@ -1,5 +1,11 @@
+// Dependencias
 const mongoose = require('mongoose');
+require('dotenv').config({
+    // Path de archivo .env
+    path: `../.env`
+})
 
-serverIp = "";
-serverPuerto = "";
-serverRuta = "";
+// Datos para la base
+serverIp = process.env.SERVERIP;
+serverPuerto = process.env.serverPuerto;
+serverRuta = process.env.serverRuta;
