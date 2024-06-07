687280308c8fcbbdceb2c30d8d532de0e1ac5cad
Nacho2001 <nachomigoni@gmail.com>
2024-06-07 13:58:20 -0300

    crud casi listo

--- Tercer Año/Desarrollo Web/appsql2/src/tasks/tasks.service.ts
+++ Tercer Año/Desarrollo Web/appsql2/src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Body, Get, Patch, Delete, Post, Param } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Task } from './entities/task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,22 +11,27 @@ export class TasksService {
     private tasksRepository: Repository<Task>,
   ){}
 
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  @Post()
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksRepository.create(createTaskDto);
   }
 
+  @Get()
   async findAll() {
     return this.tasksRepository.find()
   }
 
+  @Get(':id')
   findOne(id: number):Promise<Task> {
-    return this.tasksRepository.findOne(id)
+    return this.tasksRepository.findOne(+id);
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.tasksRepository.update(+id, updateTaskDto)
   }
 
+  @Delete()
   remove(id: number) {
     return `This action removes a #${id} task`;
   }
