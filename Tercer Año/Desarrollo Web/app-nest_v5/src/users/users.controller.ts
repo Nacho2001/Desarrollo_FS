ca2a4379b00b107e69cbc25b0890dfd505e77aad
Nacho2001 <nachomigoni@gmail.com>
2024-09-10 11:48:58 -0300

    api_user_v5 services

--- Tercer Año/Desarrollo Web/app-nest_v5/src/users/users.controller.ts
+++ Tercer Año/Desarrollo Web/app-nest_v5/src/users/users.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './users.dto';
+import { Response } from 'express';
 
 /* Al pasar "users" como al decorador, se le indica que al ingresar
 la ruta "users", se redirigirá a este controlador
 */
 @Controller('users')
 export class UsersController {
+    constructor(private readonly service: UsersService){}
+
     @Get()
     route2():string{
       return "secondary route"
+    }
+
+    @Post()
+    async register(@Body() user: UserDto, @Res() response: Response){
+      const result = await this.service.register(user);
+      response
+        .status(HttpStatus.CREATED)
+        .json({ ok: true, result, msg: 'User created!' });
     }
 }
--- Tercer Año/Desarrollo Web/app-nest_v5/src/users/users.service.ts
+++ Tercer Año/Desarrollo Web/app-nest_v5/src/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+import { QueryFailedError, Repository } from 'typeorm';
+import { UserDto } from './users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(Users) private readonly repo: Repository<UserDto>,
+    ){}
+
+    async register(user: UserDto){
+        try {
+            const result = await this.repo.save(user);
+            return result;
+        } catch (err: any) {
+            console.error(err);
+            if (err instanceof QueryFailedError)
+                throw new HttpException(`${err.name} ${err.driverError}`, 404);
+            throw new HttpException(err.message, err.status);
+        }
+    }
+}
